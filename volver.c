465eb18522eef6839936eb17aeb6c14e7f3a7481
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-06-03 01:49:16 -0500

    Corregidos pequeños detalles de la función

--- volver.c
+++ volver.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include "volver.h"
 
@@ -12,12 +13,12 @@ void volver(char *key)
         //Comprobación de si es la tecla es distinta a una letra del alfabeto
         if (!(isalpha(*key)))
         {
-            printf("No es una letra. Intenta otra vez\n");
+            printf("No es una letra. Intenta otra vez\n\n");
         }
         //Comprobación de si la tecla es distinta a la letra 'v'
         else if (!(*key == 'v' || *key == 'V'))
         {
-            printf("No es la letra 'v'. Intenta otra vez\n");
+            printf("No es la letra 'v'. Intenta otra vez\n\n");
         }
         //Se vuelve al menú principal y se limpia la consola
         else
