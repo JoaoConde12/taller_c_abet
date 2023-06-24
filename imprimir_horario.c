a6daff9d49caa3d85f7acb912f603e701db40db9
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-06-24 04:34:30 -0500

    Añadido horas al horario

--- imprimir_horario.c
+++ imprimir_horario.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "imprimir_horario.h"
 
+
 void imprimir_horario(int *option, int counter)
 {
     FILE *file;
@@ -24,12 +25,57 @@ void imprimir_horario(int *option, int counter)
                 counter++;
             }
             fclose(file);
-            printf("____________________________________________________________________________________________________________________________\n");
-            printf("                                                 Doctor %s                                                                  \n", line);
-            printf("-----------------------------------------------------------------------------------------------------------------------------\n");
-            printf(" Hora|       Lunes    |    Martes      |    Miercoles   |     Jueves     |     Viernes    |    Sabado      |     Domingo    |\n");
-            printf("-----------------------------------------------------------------------------------------------------------------------------\n");
-
+            int hour = 7;
+            int minute = 1;
+            int check_hour = 0;
+            int total_hours = 14;
+            int day = 0;
+            int total_days = 7;
+            //int value = 0;
+            int posicion = 1;
+            printf("_____________________________________________________________________________________________________________________________________\n");
+            printf("                                                         Doctor/a %s                                                                  \n", line);
+            printf("-------------------------------------------------------------------------------------------------------------------------------------\n");
+            printf("    Hora     |     Lunes      |    Martes      |    Miercoles   |     Jueves     |     Viernes    |    Sabado      |     Domingo    |\n");
+            printf("-------------------------------------------------------------------------------------------------------------------------------------\n");
+            while (hour <= total_hours)
+            {
+                if (hour < 10 && minute % 2 == 1)
+                {
+                    printf("0%d:00 - 0%d:20|", hour, hour);
+                    check_hour++;
+                }
+                if (hour < 10 && minute % 2 == 0)
+                {
+                    printf("0%d:30 - 0%d:50|", hour, hour);
+                    check_hour++;
+                }
+                if (hour >= 10 && minute % 2 == 1)
+                {
+                    printf("%d:00 - %d:20|", hour, hour);
+                    check_hour++;
+                }
+                if (hour >= 10 && minute % 2 == 0)
+                {
+                    printf("%d:30 - %d:50|", hour, hour);
+                    check_hour++;
+                }
+                while (day < total_days)
+                {
+                    printf("       -        ");
+                    printf("|");
+                    day++;
+                    posicion++;
+                }
+                day = 0;
+                minute++;
+                if(check_hour % 2 == 0)
+                {
+                    hour++;
+                }
+                printf("\n");
+            }
+            printf("-------------------------------------------------------------------------------------------------------------------------------------\n");
         }
         else
         {
