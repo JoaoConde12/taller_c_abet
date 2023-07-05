9ef16f4c74e0e50868a51b8f644a61117ed42037
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-07-05 02:04:17 -0500

    Función muestra el horario del doctor seleccionado

---
+++ visualizar_horario.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include "visualizar_horario.h"
+#include "imprimir_horario.h"
+
+void visualizar_horario(int *option)
+{
+    FILE *file;
+    int counter;
+    char path[] = "doctores.txt";
+    char line[50];
+    size_t lenght = 0;
+
+    file = fopen(path, "r");
+
+    if (file)
+    {
+        while (fgets(line, sizeof(line), file) != NULL)
+        {
+            if (line[strlen(line) - 1] == '\n')
+            {
+                line[strlen(line) - 1] == '\0';
+            }
+            printf("\t%d. %s", counter + 1, line);
+            counter++;
+        }
+    }
+    else
+    {
+        printf("No se pudo abrir el archivo");
+    }
+    printf("\n\nSeleccione un doctor: ");
+    scanf("%d", option);
+    imprimir_horario(option, counter);
+}
