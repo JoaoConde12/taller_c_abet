af62860f2497df1aa735cbd99def6b4f1a222ba6
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-07-05 03:06:13 -0500

    Añadida nueva funcion

--- obtener_informacion.h
+++ obtener_informacion.h
@@ -1,9 +1,10 @@
 #ifndef OBTENER_INFORMACION_H
 #define OBTENER_INFORMACION_H
 
-char *obtener_archivo(int counter_name_doctor);
-int **leer_horarios(char *name_file);
-void liberar_horarios(int** horario);
+char *obtener_nombre_doctor(int *option, int counter);
+char *obtener_archivo_doctor(int counter_name_doctor);
+int **leer_horario_doctor(char *name_file);
+void liberar_horario(int** horario);
 
 
 #endif
