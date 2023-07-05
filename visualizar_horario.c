53769b69d369c89621e0469f73cbb505643f3c4e
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-07-05 03:44:56 -0500

    Mejora en el input del usuario

--- visualizar_horario.c
+++ visualizar_horario.c
@@ -24,12 +24,21 @@ void visualizar_horario(int *option)
             printf("\t%d. %s", counter + 1, line);
             counter++;
         }
+        fclose(file);
     }
     else
     {
         printf("No se pudo abrir el archivo");
     }
+
     printf("\n\nSeleccione un doctor: ");
     scanf("%d", option);
+
+    if (*option < 1 || *option > counter)
+    {
+        printf("\nEse doctor no se encuentra en la lista\n\n");
+        return;
+    }
+    
     imprimir_horario(option, counter);
 }
