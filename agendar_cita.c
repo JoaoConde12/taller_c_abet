f15ffc2ca58b22657ed601716d137fe54f0ec633
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-07-10 16:42:08 -0500

    Finalizada la función para agendar_cita

--- agendar_cita.c
+++ agendar_cita.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "agendar_cita.h"
 #include "imprimir_horario.h"
-#define doctor
+#include "obtener_informacion.h"
 
 
 void agendar_cita(int *option)
@@ -12,7 +12,10 @@ void agendar_cita(int *option)
     int counter;
     char path[] = "C:\\Users\\dell-pc\\Repositorios\\taller_c_abet\\doctores.txt";
     char line[50];
-    size_t lenght = 0;
+    int row;
+    int column;
+    char *file_doctor_value;
+    int **horario;
 
     file = fopen(path, "r");
 
@@ -43,4 +46,43 @@ void agendar_cita(int *option)
     }
 
     imprimir_horario(option, counter);
+
+    printf("\nInserta el ID para seleccionar la hora (Inserta solo el numero): ");
+    scanf("%d", &row);
+    printf("\nInserta el numero del dia (Ejemplo: Lunes = 1; Martes = 2; Miercoles = 3): ");
+    scanf("%d", &column);
+    printf("\n");
+
+    file_doctor_value = obtener_archivo_doctor(*option);
+    horario = leer_horario_doctor(file_doctor_value);
+
+    if (row >= 1 && row <= 16 && column >= 1 && column <= 7)
+    {
+        if (horario[row - 1][column - 1] == 0)
+        {
+            horario[row - 1][column - 1] = 1;
+            printf("La cita ha sido agendada exitosamente.\n");
+
+            FILE *file_doctor = fopen(file_doctor_value, "w");
+            if (file_doctor)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        fprintf(file_doctor, "%d ", horario[i][j]);
+                    }
+                    fprintf(file_doctor, "\n");
+                }
+                fclose(file_doctor);
+            }
+        }
+        else
+        {
+            printf("Ese horario se encuentra ocupado.\n");
+        }
+    }
+    printf("\n");
+    
+    liberar_horario(horario);
 }
